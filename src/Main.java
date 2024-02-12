a28087d49106c9bef192d918b10707f00fbe4605
Siddharth Sahu <siddharthsahu.180@gmail.com>
2024-02-12 14:42:42 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+//class Day2{
+//    public static void main(String[] args){
+//        int a = 2;
+//        int b = 3;
+//        int sum = a + b;
+//    System.out.println(sum);
+//    }
+//}
+
+//class Day2{
+//    public static void main(String[] args){
+//        int a = 5;
+//        int b = 7;
+//        int diff = b - a;
+//        System.out.println(diff);
+//    }
+//}
+
+//class Day2{
+//    public static void main(String[] args){
+//        int a = 2;
+//        int b = 3;
+//        int mul = a*b;
+//        System.out.println(mul);
+//    }
+//}
+
+class Day2{
+    public static void main(String[] args){
+        int a = 8;
+        int b = 2;
+        int div = a/b;
+        System.out.println(div);
+    }
+}
